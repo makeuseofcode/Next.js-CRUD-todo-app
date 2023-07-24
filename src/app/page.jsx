9f1d4d2a78b786b9ff611f9e0df37326a1168bd1
utin-francis-peter <u.francispeter@gmail.com>
2023-07-24 09:35:14 +0100

    chore: cleanup

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,7 @@
 "use client";
 
 import Todos from "./components/Todos";
-import TodoContextProvider from "./context/todo.context";
 
 export default function Home() {
-  return (
-    <TodoContextProvider>
-      <Todos />
-    </TodoContextProvider>
-  );
+  return <Todos />;
 }
